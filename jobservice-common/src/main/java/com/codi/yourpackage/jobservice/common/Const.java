fc55ced879fdbb7cb642c36d7eed66923d0d29fe
chenlingjie <chenlingjie@cd121.com>
2017-09-01 09:58:33 +0800

    fc

---
+++ jobservice-common/src/main/java/com/codi/yourpackage/jobservice/common/Const.java
@@ -0,0 +1,10 @@
+package com.codi.yourpackage.jobservice.common;
+
+/**
+ * 常量
+ *
+ * @author shi.pengyan
+ * @date 2017-01-04 17:19
+ */
+public interface Const {
+}
---
+++ jobservice-common/src/main/java/com/codi/yourpackage/jobservice/common/ErrorConst.java
@@ -0,0 +1,10 @@
+package com.codi.yourpackage.jobservice.common;
+
+/**
+ * 错误常量
+ *
+ * @author shi.pengyan
+ * @date 2017-01-04 17:19
+ */
+public interface ErrorConst {
+}
